778ee949cb8254c3685e0bf8c885798529a7e45a
Surjoyday Talukdar <surjoydaytalukdar.spn@outlook.com>
2024-05-15 03:01:46 +0530

    URL location changed

--- src/assets/helper.js
+++ src/assets/helper.js
@@ -1,6 +1,6 @@
 import { TextField } from "@mui/material";
 
-const base_URL = "https://academics.newtonschool.co/api/v1/bookingportals";
+const base_URL = process.env.REACT_APP_API_URL;
 
 const PROJECT_ID = process.env.REACT_APP_PROJECT_ID;
 
